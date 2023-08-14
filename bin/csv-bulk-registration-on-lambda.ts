1c1d8350b5b83da26ab741cef0e3c9a4148fd77d
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2023-08-15 00:31:29 +0900

    add dynamodb stack

--- bin/csv-bulk-registration-on-lambda.ts
+++ bin/csv-bulk-registration-on-lambda.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CsvBulkRegistrationOnLambdaStack } from '../lib/csv-bulk-registration-on-lambda-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { CsvBulkRegistrationOnLambdaStack } from '../lib/csv-bulk-registration-on-lambda-stack'
+import { DynamoDbStack } from '../lib/dynamodb-stack'
 
-const app = new cdk.App();
-new CsvBulkRegistrationOnLambdaStack(app, 'CsvBulkRegistrationOnLambdaStack');
+const app = new cdk.App()
+
+new CsvBulkRegistrationOnLambdaStack(app, 'CsvBulkRegistrationOnLambdaStack')
+
+new DynamoDbStack(app, 'DynamoDbStack')
--- lib/csv-bulk-registration-on-lambda-stack.ts
+++ lib/csv-bulk-registration-on-lambda-stack.ts
@@ -1,19 +1,15 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
-import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class CsvBulkRegistrationOnLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
 
-    const handler = new lambdaNodejs.NodejsFunction(
-      this,
-      "CsvBulkRegistrationHandler",
-      {
-        runtime: lambda.Runtime.NODEJS_18_X,
-        entry: "lambda/handler.ts",
-      }
-    );
-  }
+        new lambdaNodejs.NodejsFunction(this, 'CsvBulkRegistrationHandler', {
+            runtime: lambda.Runtime.NODEJS_18_X,
+            entry: 'lambda/handler.ts',
+        })
+    }
 }
