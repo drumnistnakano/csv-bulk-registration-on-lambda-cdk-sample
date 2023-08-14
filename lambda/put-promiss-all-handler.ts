1018e1380d1bc8cc6bbd60ee72d5b2f2b4f0f2c0
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2023-08-15 01:28:14 +0900

    add sqs for putHandler

---
+++ lambda/put-promiss-all-handler.ts
@@ -0,0 +1,17 @@
+import {
+    APIGatewayEventRequestContext,
+    APIGatewayProxyResult,
+  } from "aws-lambda";
+  
+  exports.handler = async (
+    event: APIGatewayEventRequestContext
+  ): Promise<APIGatewayProxyResult> => {
+    console.log(JSON.stringify(event, undefined, 2));
+  
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify(event, undefined, 2),
+    };
+  };
+  
--- lib/csv-bulk-registration-on-lambda-stack.ts
+++ lib/csv-bulk-registration-on-lambda-stack.ts
@@ -2,14 +2,28 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as sqs from 'aws-cdk-lib/aws-sqs'
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources'
 
 export class CsvBulkRegistrationOnLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
 
-        new lambdaNodejs.NodejsFunction(this, 'CsvBulkRegistrationHandler', {
+        const putHandler = new lambdaNodejs.NodejsFunction(this, 'putHandler', {
             runtime: lambda.Runtime.NODEJS_18_X,
-            entry: 'lambda/handler.ts',
+            entry: 'lambda/put-handler.ts',
         })
+
+        const queueForPutHandler = new sqs.Queue(this, 'queueForPutHandler', {
+            queueName: 'queueForPutHandler',
+        })
+
+        putHandler.addEventSource(
+            new SqsEventSource(queueForPutHandler, {
+                batchSize: 1,
+                maxBatchingWindow: cdk.Duration.seconds(60),
+                maxConcurrency: 1,
+            })
+        )
     }
 }
